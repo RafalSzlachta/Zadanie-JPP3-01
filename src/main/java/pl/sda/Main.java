29773b398259660c770cf7370052be92e87abd3a
unknown <uzytkownik@o2.pl>
2020-02-23 12:35:33 +0100

    first attempt

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -1,7 +1,12 @@
 package pl.sda;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj datę");
+        
 
     }
 }
